8da9836879df837ee4c1ae0be2e263801e9dd995
Adam Wead <amsterdamos@gmail.com>
2014-11-07 14:18:38 -0500

    Enable profiling and trim browse dashboard test

--- spec/features/browse_dashboard_files_spec.rb
+++ spec/features/browse_dashboard_files_spec.rb
@@ -7,11 +7,25 @@ describe "Browse Dashboard", :type => :feature do
     sign_in FactoryGirl.create :user_with_fixtures
   end
 
-  it "should search your files by default" do
+  it "should allow you to search your files, remove constraints, and browse your files using facets" do
     visit "/dashboard"
     fill_in "q", with: "PDF"
     click_button "search-submit-header"
     expect(page).to have_content("Fake Document Title")
+    within(".constraints-container") do
+      expect(page).to have_content("You searched for:")
+      expect(page).to have_css("span.glyphicon-remove")
+      find(".dropdown-toggle").click
+    end
+    expect(page).to have_content("Fake Wav File")
+    visit "/dashboard/files"
+    click_link "Subject"
+    click_link "more Subjects"
+    click_link "consectetur"
+    within("#document_#{@fixtures[1].noid}") do
+      click_link "Display all details of Test Document MP3.mp3"
+    end
+    expect(page).to have_content("File Details")    
   end
 
   context "within my files page" do
@@ -20,7 +34,7 @@ describe "Browse Dashboard", :type => :feature do
       visit "/dashboard/files"
     end
 
-    it "should display all the necessary information" do
+    it "should display all the necessary information and refresh the page" do
       within("#document_#{@fixtures.first.noid}") do
         click_button("Select an action")
       end
@@ -32,39 +46,14 @@ describe "Browse Dashboard", :type => :feature do
       end
       expect(page).to have_link("Create Collection")
       expect(page).to have_link("Upload")
-    end
-
-    it "should allow you to search your own files and remove constraints" do
-      fill_in "q", with: "PDF"
-      click_button "search-submit-header"
-      expect(page).to have_content("Fake Document Title")
-      within(".constraints-container") do
-        expect(page).to have_content("You searched for:")
-        expect(page).to have_css("span.glyphicon-remove")
-        find(".dropdown-toggle").click
+      visit "/dashboard/files"
+      ["My Collections", "My Highlights", "Files Shared with Me"].each do |tab|
+        within("#my_nav") do
+          click_link(tab)
+        end
+        expect(page).to have_content(tab)
       end
-      expect(page).to have_content("Fake Wav File")
-    end
-
-    it "should allow you to browse facets" do
-      click_link "Subject"
-      click_link "more Subjects"
-      click_link "consectetur"
-      within("#document_#{@fixtures[1].noid}") do
-        click_link "Display all details of Test Document MP3.mp3"
-      end
-      expect(page).to have_content("File Details")
-    end
-
-    it "should allow me to edit files (from the fixtures)" do
-      fill_in "q", with: "Wav"
-      click_button "search-submit-header"
-      click_button "Select an action"
-      click_link "Edit File"
-      expect(page).to have_content("Edit Fake Wav File.wav")
-    end
-
-    it "should refresh the page of files" do
+      visit "/dashboard/files"
       click_button "Refresh"
       within("#document_#{@fixtures.first.noid}") do
         click_button("Select an action")
@@ -73,19 +62,16 @@ describe "Browse Dashboard", :type => :feature do
       end
     end
 
-    it "should allow me to edit files in batches" do
+    it "should allow me to edit files individually and in batches" do
+      fill_in "q", with: "Wav"
+      click_button "search-submit-header"
+      click_button "Select an action"
+      click_link "Edit File"
+      expect(page).to have_content("Edit Fake Wav File.wav")
+      visit "/dashboard/files"
       first('input#check_all').click
       click_button('Edit Selected')
       expect(page).to have_content('3 files')
-    end
-
-    it "should link to my other tabs" do
-      ["My Collections", "My Highlights", "Files Shared with Me"].each do |tab|
-        within("#my_nav") do
-          click_link(tab)
-        end
-        expect(page).to have_content(tab)
-      end
     end
 
   end
